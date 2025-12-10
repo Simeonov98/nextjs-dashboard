2b09110d58fc6a78197b527c03dd9fe645f68cc5
Simeonov98 <sunko1993@gmail.com>
2025-12-10 17:12:38 +0200

    render different addCard form

--- app/ui/kanaban/addCard.tsx
+++ app/ui/kanaban/addCard.tsx
@@ -9,10 +9,10 @@ import { title } from "process";
 
 
 
-export const AddCard = ({ setCards, columnId, user, title, children }: { setCards: Function; columnId: number; user: users; title: string; children: users[]}) => {
+export const AddCard = ({ setCards, columnId, user, title, children }: { setCards: Function; columnId: number; user: users; title: string; children: users[] }) => {
   const [text, setText] = useState("");
   const [adding, setAdding] = useState(false);
-  const [selectedExecutors,setSelectedExecutors] = useState<string[]>([])
+  const [selectedExecutors, setSelectedExecutors] = useState<string[]>([])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
@@ -21,60 +21,73 @@ export const AddCard = ({ setCards, columnId, user, title, children }: { setCard
     if (!text.trim().length) return;
 
     // const createdTask = await createTask(column,text,user)
-    const createdTask = await createTask(text, columnId, user,selectedExecutors)
+    const createdTask = await createTask(text, columnId, user, selectedExecutors)
     if (!createdTask) {
 
       console.error('createTask returned empty result')
       return;
     }
-     
-    // console.log("Bum!!! createTask DID NOT return empty result")
 
-    // setCards((prevCards: CardsType[]) => [...prevCards, {
-    //   id: createdTask?.id,
-    //   title: text,
-    //   columnId,
-    //   owner_id:user.id
 
-    // }]);
     setCards((prevCards: CardsType[]) => [...prevCards, createdTask]);
     setText("");
     setAdding(false);
   }
   const handleSelectedExecutors = (
-      e: React.ChangeEvent<HTMLSelectElement>
-    ) => {
-      const values = Array.from(e.target.selectedOptions, (opt) => opt.value);
-      setSelectedExecutors(values);
-    };
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const values = Array.from(e.target.selectedOptions, (opt) => opt.value);
+    setSelectedExecutors(values);
+  };
 
-  return (
-    <>
-      {adding ? (
-        <motion.form layout onSubmit={handleSubmit} >
-          <div className={`grid grid-cols-2 grid-rows-2`}>
+  const ShowExecutors = () => {
+
+    if (user.roleId < 3) {
+      return (
+              <div className={`grid grid-cols-2 grid-rows-2`}>
             <textarea
               onChange={(e) => setText(e.target.value)}
               autoFocus
               placeholder="Add new Task..."
               className="row-span-2 w-full rounded border border-violet-400 bg-violet400/20 p-3 text-sm text-neutral-450 placeholder-violet-300 focus:outline-0"
             />
+
             <div className="pl-2">
-              <select 
+              <select
                 multiple
                 className="w-full rounded border p-2 text-sm bg-white"
                 value={selectedExecutors}
                 onChange={handleSelectedExecutors}
-                >
+              >
                 {children.map((ex) => (
-              <option key={ex.id} value={ex.id}>
-                {ex.name}
-              </option>
-            ))}
+                  <option key={ex.id} value={ex.id}>
+                    {ex.name}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
-          
+      )
+    } else {
+      return (
+         <textarea
+              onChange={(e) => setText(e.target.value)}
+              autoFocus
+              placeholder="Add new Task..."
+              className="row-span-2 w-full rounded border border-violet-400 bg-violet400/20 p-3 text-sm text-neutral-450 placeholder-violet-300 focus:outline-0"
+            />
+
+      )
+    }
+  }
+
+
+  return (
+    <>
+      {adding ? (
+        <motion.form layout onSubmit={handleSubmit} >
+      
+          <ShowExecutors/>
           <div className="mt-1.5 flex items-center justify-end gap-1.5">
             <button
               onClick={() => setAdding(false)}
